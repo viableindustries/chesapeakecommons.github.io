3957aacb2728c0351809b59a810e8074ada6ad81
Joshua Powell <joshua@viableindustries.com>
2014-06-28 08:18:03 -0400

    Added base Angular installation;

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('wwwviableindustriescomApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
